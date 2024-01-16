36dd0dce6748dd0d722295759aa1e3b566288124
joshalphonse <joshalphonse@gmail.com>
2024-01-16 15:48:54 -0800

    first commit

---
+++ huggingface.py
@@ -0,0 +1,28 @@
+import bmf
+
+graph = bmf.graph()
+
+video = graph.decode({
+     "input_path": "/Users/bytedance/BMF-projects/huggingface-demo/sample.mp4"
+})
+
+bmf.encode(
+     video['video'],
+     video['audio'],
+     {
+         "output_path": "/Users/bytedance/BMF-projects/huggingface-demo",
+         "video_params": {
+             "codec": "h264",
+             "width": 320,
+             "height": 240,
+             "crf": 23,
+             "preset": "veryfast"
+         },
+         "audio_params": {
+             "codec": "aac",
+             "bit_rate": 128000,
+             "sample_rate": 44100,
+             "channels": 2
+         }
+     }
+).run()
